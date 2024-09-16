8471679df7a05c00748b0a00a97f2ae92613c41a
Maria <mariafedorova@MBP-Maria.N4>
2024-09-16 17:40:03 +0300

    change task1

--- task1/index1.py
+++ task1/index1.py
@@ -1,6 +1,6 @@
 strInput=input()
 a,b=map(int,strInput.split())
-if -10**9<=a<=10**9 and -10**9<=b<=10**9:
-  print(a+b)
-else:
-  print("Числа неверны, введите ещё раз ")
+while not(-10**9<=a<=10**9 and -10**9<=b<=10**9):
+   print("Числа неверны, введите ещё раз ")
+   a,b=map(int,strInput.split())
+print(a+b)
--- task1/index2.py
+++ task1/index2.py
@@ -1,6 +1,7 @@
 strInput=input()
 a,b=map(int,strInput.split())
-if -10**9<=a<=10**9 and -10**9<=b<=10**9:
-  print(a+b**2)
-else:
-  print("Числа неверны, введите ещё раз ")
+while not(-10**9<=a<=10**9 and -10**9<=b<=10**9):
+   print("Числа неверны, введите ещё раз ")
+   a,b=map(int,strInput.split())
+print(a+b**2)
+
